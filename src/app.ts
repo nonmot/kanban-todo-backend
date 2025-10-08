e22054b2a3301dee0db9dd3cab5bd9060203976b
nonmot <nonmot0614@gmail.com>
2025-10-08 22:40:35 +0900

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,17 @@ import userRouter from "./routes/userRoutes";
 import todoRouter from "./routes/todoRoutes"
 import authRouter from "./routes/authRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:3000'
+}
+
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api/users", userRouter);
